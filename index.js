fc11abbc18b49ccadf94c5dfded5e3a15cd027c6
Rafael Mesquita <rgalvaomesquita@gmail.com>
2020-04-07 15:21:04 -0300

    Mudando para utilizar o banco no heroku

--- index.js
+++ index.js
@@ -2,31 +2,34 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const path = require('path')
+
+
+
+/*
 var pg = require('pg')
 var conString = "postgres://localhost/beautycities";
-
 const config = {
   user: 'postgres',
   database: 'beautycities',
   password: 'admin12345',
   port: 5432                  //Default port, change it if needed
 };
-
 var pool = new pg.Pool(config)
+*/
 
-// connection using created pool
-/*pool.connect(function(err, client, done) {
 
-  if (err) {
-    return console.error('error running query', err);
-  }else{
-    client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-      done();
-      
-      console.log(result.rows[0].number);
-    });
-  }
-})*/
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+
+
 
 
 app.set('view engine', 'ejs')
@@ -41,9 +44,9 @@ app.listen(port, (err) => {
 })
 
 app.get('/', async(request,response) => {
-    const client = await pool.connect()
+    //const client = await pool.connect()
     try {
-      const cidades = await pool.query('SELECT * from cidades order by name;')
+      const cidades = await client.query('SELECT * from cidades order by name;')
       
       response.render('home', {
         cidades
@@ -61,11 +64,11 @@ app.get('/cidade/:id',async(request,response) => {
   //const client = await pool.connect()
   try {
     
-    const ruas = await pool.query(`select * from ruas where fk_cidade_id = $1::integer`, [request.params.id])
+    const ruas = await client.query(`select * from ruas where fk_cidade_id = $1::integer`, [request.params.id])
     
-    const cidade = await pool.query(`select name from cidades where id = $1::integer`, [request.params.id])
+    const cidade = await client.query(`select name from cidades where id = $1::integer`, [request.params.id])
     
-    const locations = await pool.query(`select * from locations where id_cidade = $1::integer`, [request.params.id])
+    const locations = await client.query(`select * from locations where id_cidade = $1::integer`, [request.params.id])
     
     response.render('cidade', {cidade,ruas,locations})
     
