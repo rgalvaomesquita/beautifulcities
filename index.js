135877c2e7e0f4dbb92b6034b912a2ecccb43805
Rafael Mesquita <rgalvaomesquita@gmail.com>
2020-04-07 22:36:32 -0300

    Modificando conexão com banco para execução no heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 
 
-var pg = require('pg')
+/*var pg = require('pg')
 var conString = "postgres://ejnktmfophwmce:c0b0605fb928c58dfd4c41ff54b9ca11e97904c410ac08a02c18e1e04c5da6a2@ec2-3-91-112-166.compute-1.amazonaws.com:5432/d1kktm0euitb3a?ssl=true";
 const config = {
   user: "ejnktmfophwmce",
@@ -16,7 +16,16 @@ const config = {
   host: "ec2-3-91-112-166.compute-1.amazonaws.com",
   ssl: true
 };
-var pool = new pg.Pool(config)
+var pool = new pg.Pool(config)*/
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
 
 
 
@@ -48,7 +57,7 @@ app.listen(port, (err) => {
 app.get('/', async(request,response) => {
     //const client = await pool.connect()
     try {
-      const cidades = await pool.query('SELECT * from cidades order by name;')
+      const cidades = await client.query('SELECT * from cidades order by name;')
       
       response.render('home', {
         cidades
@@ -66,11 +75,11 @@ app.get('/cidade/:id',async(request,response) => {
   //const client = await pool.connect()
   try {
     
-    const ruas = await pool.query(`select * from ruas where fk_cidade_id = $1::integer`, [request.params.id])
+    const ruas = await client.query(`select * from ruas where fk_cidade_id = $1::integer`, [request.params.id])
     
-    const cidade = await pool.query(`select name from cidades where id = $1::integer`, [request.params.id])
+    const cidade = await client.query(`select name from cidades where id = $1::integer`, [request.params.id])
     
-    const locations = await pool.query(`select * from locations where id_cidade = $1::integer`, [request.params.id])
+    const locations = await client.query(`select * from locations where id_cidade = $1::integer`, [request.params.id])
     
     response.render('cidade', {cidade,ruas,locations})
     
